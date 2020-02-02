96d55a009d395de60bbe875b568ba4e90d7944f9
Aldirus <soakes68@hotmail.ca>
2020-02-02 18:14:43 -0400

    1.2

--- DiceRoller/app/src/main/java/com/nbcc/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/nbcc/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nbcc.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+       // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+
+        resultText.text = randomInt.toString()
     }
 }
